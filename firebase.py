3f08b9cf04dd138fe9e1da76f5d3b59bc7ea7bc6
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-08-23 23:12:52 +1200

    Update firebase.py
    - Updated database url change
    - Added and ran test to add to database
    - Commented out functions not in use for testing

--- firebase.py
+++ firebase.py
@@ -2,21 +2,29 @@
 import pyrebase
 
 config = {
-    "apiKey": "AIzaSyDch2JUJGJCo6jo9XemL9PO5TJLf-wYnx8",
-    "authDomain": "revercial-e4094.firebaseapp.com",
-    "databaseURL": "https://revercial-e4094-default-rtdb.firebaseio.com",
-    "projectId": "revercial-e4094",
-    "storageBucket": "revercial-e4094.appspot.com",
-    "messagingSenderId": "890528169241",
-    "appId": "1:890528169241:web:57e71d0f847625eb56d988",
-    "measurementId": "G-JSRGENNMEJ"
+    "databaseURL": "https://revercial-43fe3-default-rtdb.firebaseio.com/",
+    "apiKey": "AIzaSyBd7z644_giN5TX9lv5T1AdHfp1ilwsrbA",
+    "authDomain": "revercial-43fe3.firebaseapp.com",
+    "projectId": "revercial-43fe3",
+    "storageBucket": "revercial-43fe3.appspot.com",
+    "messagingSenderId": "277923181143",
+    "appId": "1:277923181143:web:17230c5cdb451126146912",
+    "measurementId": "G-4VYLQMSHKR"
 }
 
 firebase = pyrebase.initialize_app(config)
-db = firebase.database
+db = firebase.database()
 
-def save_profile():
-    print("Profile saved.")
+# For test adding a user to the database
+firstName = "Karl"
+surname = "Francisco"
+name = surname + "." + firstName
+data = {"Name": firstName, "Surname": surname, "Date of Birth": "26/04/1995", "Age": 28}
+db.child("Users").child(name).set(data)
 
-def update_profile():
-    print("Profile updated.")
+# Functions
+# def save_profile():
+#     print("Profile saved.")
+
+# def update_profile():
+#     print("Profile updated.")
