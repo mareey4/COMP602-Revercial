a679a059cf1401e763d4a014ce31169540a73069
Maria Paule <132525779+mareey4@users.noreply.github.com>
2023-08-28 10:26:45 +1200

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Background from "./Components/Background";
+import TextComponent from "./Components/Text";
+
+const App = () => {
+  return <TextComponent />;
+};
+
+export default App;
---
+++ Text.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Text.css";
+
+const Text = () => {
+  return (
+    <div className="text-container">
+      <div className="wrapper">
+        <h1>Create Account</h1>
+      </div>
+      <div className="wrapper-2">
+        <h2>First Name</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Text;
