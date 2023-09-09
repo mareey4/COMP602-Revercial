cad231771bddc847b5c2337910b7e730d010e2b6
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-09-09 12:23:47 +1200

    Update firebase.js
    - Added a checkExistingEmail(String email) function to check for any existing accounts using the given email for creating a new account.
    - Added a getAllUsers() function to return an array of user objects for the friends list and messaging user stories.

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp }  from "firebase/app";
-import {getDatabase, ref, set } from "firebase/database";
+import {getDatabase, ref, set, onValue } from "firebase/database";
+import User from './user';
 
 const config = {
   apiKey: "AIzaSyBd7z644_giN5TX9lv5T1AdHfp1ilwsrbA",
@@ -17,13 +18,60 @@ const fbConfig = initializeApp(config);
   export function saveUserData(newUser) {
     const db =  getDatabase();
     const sanitizedEmail = newUser.email.replace(".",",");
-    const reference = ref(db, 'Users/' + sanitizedEmail);
+    const referenceEmail = ref(db, 'Users/' + sanitizedEmail);
 
-    set(reference, {
+    set(referenceEmail, {
         Name: newUser.first_name,
         Surname: newUser.surname,
         DOB: newUser.date_of_birth,
         Username: newUser.username,
         Password: newUser.password
     });
+  }
+
+  export function checkExistingEmail(userEmail) {
+    const db = getDatabase();
+    const refEmail = ref(db, 'Users');
+    const sanitizedEmail = userEmail.replace(".",",");
+    let exists = false;
+
+    onValue(refEmail, (snapshot) => {
+      const dataEmail = snapshot.val();
+
+      for(const email in dataEmail){
+        if(email === sanitizedEmail){
+          exists = true;
+          break;
+        }
+      }
+    });
+
+    return exists;
+  }
+
+  export function getAllUsers() {
+    const db = getDatabase();
+    const refUsers = ref(db, 'Users');
+    const users = [];
+
+    onValue(refUsers, (snapshot) => {
+      const data = snapshot.val();
+
+      for(const email in data){
+        const userData = data[email];
+
+        const user = new User(
+          userData.Name,
+          userData.Surname,
+          userData.Username,
+          userData.DOB,
+          email,
+          userData.password
+        );
+
+        users.push(user);
+      }
+    });
+
+    return users;
   }
