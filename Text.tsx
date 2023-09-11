5b9500d9abc50e10e63cfbb3556cd736b47df341
Maria Paule <132525779+mareey4@users.noreply.github.com>
2023-09-11 19:27:09 +1200

    Add files via upload

--- Text.tsx
+++ Text.tsx
@@ -1,20 +1,40 @@
 import "./Text.css";
-import { checkExistingEmail, saveUserData, getAllUsers } from "./firebase";
+import { saveUserData, checkExistingEmail, getAllUsers } from "./firebase";
 import User from "./user";
-
+/* import { useHistory } from "react-router-dom";
+ */
 function Text() {
-  const handleCreateClick = () => {
+  /*   const history = useHistory();
+   */ const handleCreateClick = () => {
     console.log("Create button clicked");
 
-    const firstNameInput = document.querySelector('input[name="fname"]') as HTMLInputElement;
-    const lastNameInput = document.querySelector('input[name="lname"]') as HTMLInputElement;
-    const dobInput = document.querySelector('input[name="dob"]') as HTMLInputElement;
-    const emailInput = document.querySelector('input[name="email"]') as HTMLInputElement;
-    const usernameInput = document.querySelector('input[name="username"]') as HTMLInputElement;
-    const passwordInput = document.querySelector('input[name="password"]') as HTMLInputElement;
+    const firstNameInput = document.querySelector(
+      'input[name="fname"]'
+    ) as HTMLInputElement;
+    const lastNameInput = document.querySelector(
+      'input[name="lname"]'
+    ) as HTMLInputElement;
+    const dobInput = document.querySelector(
+      'input[name="dob"]'
+    ) as HTMLInputElement;
+    const emailInput = document.querySelector(
+      'input[name="email"]'
+    ) as HTMLInputElement;
+    const usernameInput = document.querySelector(
+      'input[name="username"]'
+    ) as HTMLInputElement;
+    const passwordInput = document.querySelector(
+      'input[name="password"]'
+    ) as HTMLInputElement;
 
-    if (!firstNameInput.value || !lastNameInput.value || !dobInput.value || 
-      !emailInput.value || !usernameInput.value || !passwordInput.value) {
+    if (
+      !firstNameInput ||
+      !lastNameInput ||
+      !dobInput ||
+      !emailInput ||
+      !usernameInput ||
+      !passwordInput
+    ) {
       alert("Please fill in all fields.");
       return;
     }
@@ -38,44 +58,53 @@ function Text() {
       );
 
       saveUserData(newUser);
-
+      /*       history.push("srcComponentsProfile.tsx");
+       */
     } else {
-      alert("Existing account with the given email already exists.")
+      alert("Existing account with the given email already exists.");
     }
   };
 
   return (
-    <div className="text-container">
-      <div className="wrapper">
-        <h1>Create Account</h1>
-      </div>
-      <div className="wrapper-2">
-        <label>
-          First Name: <input name="fname" /> Last Name: <input name="lname" />
-        </label>
-        <div>
-          <label>
-            DOB: <input type="date" name="dob" />
-          </label>
+    <body>
+      <div>
+        <div className="wrapper">
+          <h1>Create Account</h1>
         </div>
-        <div>
+        <div className="wrapper-2">
           <label>
-            Email: <input name="email" />
+            First Name: <input name="fname" /> Last Name: <input name="lname" />
           </label>
-        </div>
-        <div>
-          <label>
-            Username: <input name="username" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password: <input type="password" name="password" />
-          </label>
-          <p className="password-requirements">
-            Password must have an uppercase letter, a lowercase letter, a
-            number, and be at least 8 characters long.
-          </p>
+          <div>
+            <label>
+              DOB: <input type="date" name="dob" />
+            </label>
+          </div>
+          <div>
+            <label>
+              Email: <input name="email" />
+            </label>
+          </div>
+          <div>
+            <label>
+              Username: <input name="username" />
+            </label>
+          </div>
+          <div>
+            <label>
+              Password: <input type="password" name="password" />
+            </label>
+            <p className="password-requirements">
+              Password must have an uppercase letter, a lowercase letter, a
+              number, and be at least 8 characters long.
+            </p>
+          </div>
+          <div className="login-link">
+            <p>
+              Already have an account?{" "}
+              <a href="src\Components\login.html">Login here</a>
+            </p>
+          </div>
         </div>
         <div className="create-button-container">
           <button className="create-button" onClick={handleCreateClick}>
@@ -83,7 +112,7 @@ function Text() {
           </button>
         </div>
       </div>
-    </div>
+    </body>
   );
 }
 
