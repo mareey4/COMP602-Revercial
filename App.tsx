a8389927ffc24b05cd2d1815b8d3675bc064b1c8
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-09-13 22:17:04 +1200

    Update App.tsx
    Updated to use the converted login page files, Login.tsx and Login.css

--- App.tsx
+++ App.tsx
@@ -3,18 +3,18 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate,
 } from "react-router-dom";
 import TextComponent from "./Components/Text"; // Import your create account component
+import LoginComponent from "./Components/Login"; // Import your Login component
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<TextComponent />} />
         <Route path="/create-account" element={<TextComponent />} />
-        <Route path="/login" element={<Navigate to="/login.html" />} />
+        <Route path="/login" element={<LoginComponent />} />
       </Routes>
-      <TextComponent />
     </Router>
   );
 };
