e4b2eb2eb248f770a1308c9d83a1a5b3af73aa70
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-09-14 16:01:52 +1200

    Update App.tsx
    Updated, to use both Login.tsx and Maria's Profile page

--- App.tsx
+++ App.tsx
@@ -3,18 +3,19 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate,
 } from "react-router-dom";
 import TextComponent from "./Components/Text"; // Import your create account component
-import Profile from "./Components/Profile";
+import Profile from "./Components/Profile"; // Import your Profile component
+import LoginComponent from "./Components/Login"; // Import your Login component
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<TextComponent />} />
+        <Route path="/create-account" element={<TextComponent />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Navigate to="/login.html" />} />
+        <Route path="/login" element={<LoginComponent />} />
       </Routes>
     </Router>
   );
