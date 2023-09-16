2df85fbaef7154c4010ae53a90df826cccc47c7d
Benny-Lu <61341074+Benny-Lu@users.noreply.github.com>
2023-09-16 16:18:38 +1200

    Update Login.tsx
    When user login successfully they will be directed to a profile page

--- Login.tsx
+++ Login.tsx
@@ -1,16 +1,23 @@
 import "./style.css";
 import { login } from "./validation.js";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   const handleLoginClick = async () => {
-    const id = document.querySelector('input[name="username"]') as HTMLInputElement;
-    const password = document.querySelector('input[name="password"]') as HTMLInputElement;
+    const id = document.querySelector(
+      'input[name="username"]'
+    ) as HTMLInputElement;
+    const password = document.querySelector(
+      'input[name="password"]'
+    ) as HTMLInputElement;
 
     try {
       const validLogin = await login(id.value, password.value);
 
       if (validLogin) {
         alert("Login Successful");
+        navigate("/Profile");
       } else {
         alert("Failed to login");
       }
@@ -28,9 +35,19 @@ const Login = () => {
           <input type="text" name="username" placeholder="Username" required />
         </div>
         <div className="input-box">
-          <input type="password" name="password" placeholder="Password" required />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
         </div>
-        <button type="button" id="loginButton" className="btn" onClick={handleLoginClick}>
+        <button
+          type="button"
+          id="loginButton"
+          className="btn"
+          onClick={handleLoginClick}
+        >
           Login
         </button>
         <div className="forgot-links">
