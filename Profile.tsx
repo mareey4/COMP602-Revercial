83c01f4f97c8d234c0057fe5d79f15cac6ac2559
Benny-Lu <61341074+Benny-Lu@users.noreply.github.com>
2023-09-21 23:33:32 +1200

    Update Profile.tsx
    Updated profile with username, name, and profile picture

--- Profile.tsx
+++ Profile.tsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
-import { useLocation, Link } from "react-router-dom"; // Import Link from React Router
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getProfilePic } from "./validation";
 import "./Profile.css";
 import "./NavBar.css";
+import { useLocation } from "react-router-dom";
 
 function Profile() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -47,8 +48,7 @@ function Profile() {
         <h2></h2>
         <ul>
           <li>
-            <Link to="/create-events">Create Event</Link>{" "}
-            {/* Link to CreateEvents */}
+            <Link to="/create-events">Create Event</Link>
           </li>
           <li>
             <a href="#">Settings</a>
@@ -57,20 +57,27 @@ function Profile() {
             <Link to="/Privacy">Privacy Settings</Link>
           </li>
           <li>
-            <Link to="/Login" onClick={handleLogout}>Log out</Link>{" "}
+            <Link to="/Support">Support Page</Link>
+          </li>
+          <li>
+          <Link to="/Login" onClick={handleLogout}>Log out</Link>{" "}
           </li>
         </ul>
       </div>
 
       {/* Main Content */}
       <div className="main-content">
-        <h1>Profile</h1>
-
-        {/* Display the profile picture */}
         {profilePicUrl && (
-          <img src={profilePicUrl} alt="Profile" className="profile-pic" />
-        )}
+        <div className="profile-info">
+      <img src={profilePicUrl} alt="Profile" className="profile-pic" />
+      <div className="user-details">
+        <p className="user-name">{user.first_name}</p>
+        <p className="user-username">@{user.username}</p>
       </div>
+    </div>
+  )}
+  <h1>Profile</h1>
+</div>
     </div>
   );
 }
