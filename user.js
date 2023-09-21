de46ee305e157b1f731ace4df49122ef900919fb
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-09-21 12:29:48 +1200

    Update user.js
    - Added a variable to hold login status flag for Login page to check if user input the correct password.
    - Added a variable to hold the profile picture of the user.

--- user.js
+++ user.js
@@ -1,21 +1,19 @@
 class User {
-    constructor(first_name, surname, username, date_of_birth, email, password) {
-      this.first_name = first_name;
-      this.surname = surname;
-      this.username = username;
-      this.date_of_birth = date_of_birth;
-      this.email = email;
-      this.password = password;
+  constructor(first_name, surname, username, date_of_birth, email, password, loginStatus, profilePic) {
+    this.first_name = first_name;
+    this.surname = surname;
+    this.username = username;
+    this.date_of_birth = date_of_birth;
+    this.email = email;
+    this.password = password;
+    this.loginStatus = loginStatus;
+    if(profilePic !== undefined || profilePic !== null) {
+      this.profilePic = profilePic;
+    } else {
+      this.profilePic = undefined;
     }
   }
-  
-  // Example usage:
-  const user = new User(
-    "John",
-    "Doe",
-    "johndoe",
-    new Date("1990-01-01"),
-    "john@example.com",
-    "password123"
-  );
+}
+
+export default User;
   
