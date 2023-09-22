c5ac85868a9c1066996ba4f811dcd6add8ea1430
Maria Paule <132525779+mareey4@users.noreply.github.com>
2023-09-22 19:02:21 +1200

    Update TicketGenerator.js

--- TicketGenerator.js
+++ TicketGenerator.js
@@ -1,26 +1,31 @@
 import { fbConfig } from './firebase';
 import { getDatabase, ref, set, onValue, remove } from 'firebase/database'
 
-
+// Function to generate a random ticket ID
 export async function generateTicketID() {
+        // Define characters and length for the ticket ID
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         const length = 8;
         let ticketID = '';
 
+        // Generate a random ticket ID
         return new Promise((resolve) => {
             for(let i = 0; i < length; i++) {
                 const randIndex = Math.floor(Math.random() * characters.length);
                 ticketID += characters.charAt(randIndex);
             }
 
-            resolve(ticketID);
+            resolve(ticketID); // Resolve the promise with the generated ticket ID
             return;
         });
     }
 
+// Function to check if a ticket ID exists in Firebase database
     export async function checkExistingTicketID(ticketID) {
         const db = getDatabase(fbConfig);
-        const topicOptions = [
+            
+          // Define a list of topic options
+            const topicOptions = [
             'Account Issues',
             'Privacy and Security',
             'Content Management',
@@ -30,14 +35,16 @@ export async function generateTicketID() {
             'Report Technical Glitches',
             'Other'
         ];
-    
+
+         // Create an array of promises to check ticket ID existence for each topic
         const promises = topicOptions.map(async (topicOptions) => {
             const refSubject = databaseRef(db, 'Support/' + topicOptions);
             const refTicket = child(refSubject, ticketID);
             const snapshot = await get(refTicket);
             return snapshot.exists();
         });
-    
+
+        // Wait for all promises to resolve and check if at least one topic contains the ticket ID
         const result = await Promise.all(promises);
         const validID = result.some((exists) => exists);
     
