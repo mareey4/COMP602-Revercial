02e31ca7592119abeed01eea6f3148e6ead6c2e1
Benny-Lu <61341074+Benny-Lu@users.noreply.github.com>
2023-09-22 19:17:21 +1200

    Update Login.tsx
    Add comments

--- Login.tsx
+++ Login.tsx
@@ -1,32 +1,42 @@
-import "./style.css";
+import "./Login.css";
 import { login } from "./validation.js";
 import { Link, useNavigate } from "react-router-dom";
 
+// Define the Login component.
 const Login = () => {
+  // Initialize the navigate function for programmatic navigation.
   const navigate = useNavigate();
+
+  // Function to handle the login button click.
   const handleLoginClick = async () => {
-    const id = document.querySelector(
-      'input[name="username"]'
-    ) as HTMLInputElement;
-    const password = document.querySelector(
-      'input[name="password"]'
-    ) as HTMLInputElement;
+    // Get the input values for username and password.
+    const id = document.querySelector('input[name="username"]') as HTMLInputElement;
+    const password = document.querySelector('input[name="password"]') as HTMLInputElement;
 
     try {
-      const validLogin = await login(id.value, password.value);
+      // Call the login function with provided credentials.
+      let resultUser = await login(id.value, password.value);
 
-      if (validLogin) {
-        alert("Login Successful");
-        navigate("/Profile");
+      if (resultUser !== undefined) {
+        if (resultUser.loginStatus) {
+          // If login is successful, show an alert and navigate to the profile page.
+          alert("Login Successful");
+          navigate("/profile", { state: { user: resultUser } });
+        } else {
+          // If the password is wrong, show an alert.
+          alert("Wrong password!");
+        }
       } else {
-        alert("Failed to login");
+        // If the account does not exist, show an alert.
+        alert("Account does not exist!");
       }
     } catch (error) {
-      // Handle error here
+      // Handle any errors that occur during the login process.
       console.error(error);
     }
   };
 
+  // Render the Login component.
   return (
     <div className="wrapper">
       <form>
@@ -35,27 +45,14 @@ const Login = () => {
           <input type="text" name="username" placeholder="Username" required />
         </div>
         <div className="input-box">
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-          />
+          <input type="password" name="password" placeholder="Password" required />
         </div>
-        <button
-          type="button"
-          id="loginButton"
-          className="btn"
-          onClick={handleLoginClick}
-        >
+        <button type="button" id="loginButton" className="btn" onClick={handleLoginClick}>
           Login
         </button>
-        <div className = "need-account">
-          <p>
-            Don't have an account?
-            <Link to="/create-account"> Sign up here!</Link>
-          </p>
-        </div>
+        <li>
+          <Link to="/create-account">Don't have an account?</Link>
+        </li>
         <div className="forgot-links">
           <a href="#">Forgot Username or Forgot Password</a>
         </div>
@@ -64,4 +61,5 @@ const Login = () => {
   );
 };
 
+// Export the Login component as the default export.
 export default Login;
