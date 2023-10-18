93a1c221fb33d76f77055afa75b63db0ec3a828d
Benny-Lu <61341074+Benny-Lu@users.noreply.github.com>
2023-10-18 20:04:03 +1300

    Update Privacy.tsx
    Added a bio in profile

--- Privacy.tsx
+++ Privacy.tsx
@@ -1,14 +1,74 @@
-import { useState } from "react";
-import { Link } from "react-router-dom"; // Import Link from React Router
-import "./privacy.css";
+import { useEffect, useState } from "react";
+import { Link, useNavigate} from "react-router-dom";
+import { saveUserBio, getProfilePic } from "./validation";
+import "./Profile.css";
 import "./NavBar.css";
+import { useLocation } from "react-router-dom";
 
-function Privacy() {
+function Profile() {
+
+  // State to control the sidebar's open/close status.
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
+  const navigate = useNavigate();
+  
+
+  // State to store the profile picture URL.
+  const [profilePicUrl, setProfilePicUrl] = useState(null);
+
+  // Get the user data from the location state.
+  const location = useLocation();
+  const user = location.state?.user;
+
+  // Sanitize email and profile picture name for fetching the profile picture.
+  const unsanitizedEmail = user.email.replaceAll(",", ".");
+  const unsanitizedPFPName = user.profilePic.replaceAll(",", ".");
+
+  // Add a state variable for the user's bio
+  const [bio, setBio] = useState(user.bio || ""); // Initialize with the user's existing bio, if available
+  console.log("User Data:", user);
+
+  const maxCharacterLimit = 500;
+
+  // useEffect to load the user's profile picture.
+  useEffect(() => {
+    async function loadProfilePic() {
+      const url = await getProfilePic(unsanitizedEmail, unsanitizedPFPName);
+      if (url) {
+        setProfilePicUrl(url);
+      }
+    }
+
+    loadProfilePic();
+  }, [unsanitizedEmail, unsanitizedPFPName]);
+
+  // Function to toggle the sidebar open/close status.
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
+
+  // Function to handle user logout (not fully implemented).
+  const handleLogout = () => {
+    if (user) {
+      user.loginStatus = false; // Update the login status (Note: This might need further implementation).
+    }
+  }
+
+  const handleBioChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    // Update the bio state when the input changes
+    setBio(event.target.value);
+  }
+
+  const handleSaveBio = () => {
+    // Call the saveUserBio function to save the user's bio
+    saveUserBio(user.email, bio);
+    alert('Bio saved successfully!');
+  }
+
+  const handleEventsLink = () =>{
+    navigate("/profile", { state: { user: user } });
+  }
+  
 
   return (
     <div className={`profile-container ${sidebarOpen ? "sidebar-open" : ""}`}>
@@ -24,8 +84,7 @@ function Privacy() {
         <h2></h2>
         <ul>
           <li>
-            <Link to="/create-events">Create Event</Link>{" "}
-            {/* Link to CreateEvents */}
+            <Link to="/create-events" onClick={handleEventsLink}>Create Event</Link>
           </li>
           <li>
             <a href="#">Settings</a>
@@ -34,111 +93,45 @@ function Privacy() {
             <Link to="/Privacy">Privacy Settings</Link>
           </li>
           <li>
-            <Link to="/Login">Log out</Link>{" "}
+            <Link to="/Support">Support Page</Link>
+          </li>
+          <li>
+            <Link to="/Login" onClick={handleLogout}>Log out</Link>{" "}
           </li>
         </ul>
       </div>
 
       {/* Main Content */}
-      <div>
-      <header className="header">
-        <h1 className="header__title">Profile Privacy Settings</h1>
-        <p className="header__description">
-          Welcome to your account privacy settings!
-        </p>
-      </header>
-
-      <section className="privacy-options">
-        <h2 className="privacy-options__title">Privacy Options</h2>
-        <div className="privacy-container">
-          <form action="#" method="POST">
-            <div className="privacy-option">
-              <label htmlFor="profileVisibility" className="privacy-option__label">
-                Profile Visibility:
-              </label>
-              <select
-                name="profileVisibility"
-                id="profileVisibility"
-                className="privacy-option__select"
-              >
-                <option value="public">Public</option>
-                <option value="private">Private</option>
-                <option value="friends">Friends Only</option>
-              </select>
-              <p className="privacy-option__description">
-                Who can see your profile page?
-              </p>
+      <div className="main-content">
+        {profilePicUrl && (
+          <div className="profile-info">
+            <img src={profilePicUrl} alt="Profile" className="profile-pic" />
+            <div className="user-details">
+              <p className="user-name">{user.first_name}</p>
+              <p className="user-username">@{user.username}</p>
             </div>
+          </div>
+        )}
 
-            <div className="privacy-line"></div>
-
-            <div className="privacy-option">
-              <label htmlFor="postVisibility" className="privacy-option__label">
-                Post Visibility:
-              </label>
-              <select
-                name="postVisibility"
-                id="postVisibility"
-                className="privacy-option__select"
-              >
-                <option value="public">Public</option>
-                <option value="private">Private</option>
-                <option value="friends">Friends Only</option>
-              </select>
-              <p className="privacy-option__description">Who can see your posts?</p>
+        
+        <h1>Profile</h1>
+        <div className="bio-section">
+          <h2>Bio</h2>
+          <textarea
+            className="bio-textarea"
+            value={bio}
+            onChange={handleBioChange}
+            placeholder="Type your bio here"
+            maxLength={maxCharacterLimit} // Set the maximum character limit
+          />
+          <div className="char-counter">
+            Charcter Count: {bio.length}/{maxCharacterLimit}
             </div>
-
-            <div className="privacy-line"></div>
-
-            <div className="privacy-option">
-              <label htmlFor="contactPreference" className="privacy-option__label">
-                Who can contact me:
-              </label>
-              <select
-                name="contactPreference"
-                id="contactPreference"
-                className="privacy-option__select"
-              >
-                <option value="everyone">Everyone</option>
-                <option value="friends">Friends Only</option>
-                <option value="nobody">Nobody</option>
-              </select>
-              <p className="privacy-option__description">
-                Who can send you messages or friend requests?
-              </p>
-            </div>
-
-            <div className="privacy-line"></div>
-
-            <div className="privacy-option">
-              <label htmlFor="findPreference" className="privacy-option__label">
-                How people can find you:
-              </label>
-              <select
-                name="findPreference"
-                id="findPreference"
-                className="privacy-option__select"
-              >
-                <option value="search">Allow search</option>
-                <option value="friendRequests">Only through friend requests</option>
-                <option value="nobody">Nobody can find me</option>
-              </select>
-              <p className="privacy-option__description">
-                How can people discover your profile?
-              </p>
-            </div>
-
-            <div className="privacy-line"></div>
-
-            <button type="submit" className="privacy-options__save-button">
-              Save Changes
-            </button>
-          </form>
+          <button onClick={handleSaveBio}>Save Bio</button>
         </div>
-      </section>
-    </div>
+      </div>
     </div>
   );
 }
 
-export default Privacy;
+export default Profile;
