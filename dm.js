96761937356709b9982572257c14b8eb25406dda
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-10-24 19:24:03 +1300

    Add files via upload
    Constructor file for messages

---
+++ dm.js
@@ -0,0 +1,9 @@
+class DM {
+    constructor(recipient, sender, message) {
+      this.recipient = recipient;
+      this.sender = sender;
+      this.message = message;
+    }
+}
+  
+export default DM;
